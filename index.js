a8971030a0038ca1cf0ccc4467de77cccfb00723
Jan Antala <jan@antala.sk>
2014-05-27 14:43:24 +0200

    feat: log request url and body

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var fs = require('fs');
 var app = express();
+
+// simple logger
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+// parse application/json and application/x-www-form-urlencoded
+app.use(bodyParser());
+
 
 var read_json = function read_json(filename, default_value) {
   try {
@@ -13,22 +24,26 @@ var read_json = function read_json(filename, default_value) {
 var path = 'json';
 
 app.get('/', function(req, res){
+  console.dir(req.query);
   var data =  read_json(path + 'index' + '.get.json');
   res.json(data);
 });
 
 app.get('/:path', function(req, res){
-  var data =  read_json(path + req.originalUrl + '.get.json');
+  console.dir(req.query);
+  var data =  read_json(path + req.path + '.get.json');
   res.json(data);
 });
 
 app.post('/', function(req, res){
+  console.dir(req.body);
   var data =  read_json(path + 'index' + '.post.json');
   res.json(data);
 });
 
-app.post('/', function(req, res){
-  var data =  read_json(path + req.originalUrl + '.post.json');
+app.post('/:path', function(req, res){
+  console.dir(req.body);
+  var data =  read_json(path + req.path + '.post.json');
   res.json(data);
 });
 
@@ -37,18 +52,21 @@ app.put('/', function(req, res){
   res.json(data);
 });
 
-app.put('/', function(req, res){
-  var data =  read_json(path + req.originalUrl + '.put.json');
+app.put('/:path', function(req, res){
+  console.dir(req.body);
+  var data =  read_json(path + req.path + '.put.json');
   res.json(data);
 });
 
 app.delete('/', function(req, res){
+  console.dir(req.body);
   var data =  read_json(path + 'index' + '.delete.json');
   res.json(data);
 });
 
-app.delete('/', function(req, res){
-  var data =  read_json(path + req.originalUrl + '.delete.json');
+app.delete('/:path', function(req, res){
+  console.dir(req.body);
+  var data =  read_json(path + req.path + '.delete.json');
   res.json(data);
 });
 
